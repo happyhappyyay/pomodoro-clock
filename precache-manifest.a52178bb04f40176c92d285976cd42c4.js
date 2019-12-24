6ee0c0e07a7ebf718228d364ad30b365ace54d30
happyhappyyay <chapan.denis@gmail.com>
2019-12-23 22:12:15 -0600

    Updates

---
+++ precache-manifest.a52178bb04f40176c92d285976cd42c4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "51e7c6eb5f96d763a0b11d01309c01d9",
+    "url": "./index.html"
+  },
+  {
+    "revision": "c0ad2e464d12943c83dc",
+    "url": "./static/js/2.851d0187.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "./static/js/2.851d0187.chunk.js.LICENSE"
+  },
+  {
+    "revision": "408e2ecc6b07c74c06b1",
+    "url": "./static/js/main.44433415.chunk.js"
+  },
+  {
+    "revision": "c20656c30f6741c7d73a",
+    "url": "./static/js/runtime-main.da43e112.js"
+  }
+]);
